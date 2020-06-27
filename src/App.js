b2767c49099d3f9634400fd2885b32867ec2660a
tim <timothy.potts@colorado.edu>
2020-06-27 16:18:49 -0600

    making some headway

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import TextPost from "./components/TextPost";
+import { Row, Col } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Row>
+        <Col lg="10" md="10" sm="10" xs="12">
+          <TextPost />
+        </Col>
+      </Row>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from "reactstrap";
+
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" light expand="md">
+        <NavbarBrand className="text-success" href="/">
+          workingTitle
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret className="text-info">
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavLink className="text-info" href="/register/">
+            Sign Up
+          </NavLink>
+          <NavLink className="text-info" href="/login/">
+            Sign In
+          </NavLink>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
