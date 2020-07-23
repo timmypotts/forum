e063c258866cf169982faed73f52fcb6e56d4e41
tim <timothy.potts@colorado.edu>
2020-07-23 17:06:30 -0600

    making some headway

--- api/app.js
+++ api/app.js
@@ -10,6 +10,9 @@ var testAPIRouter = require("./routes/testAPI");
 
 var app = express();
 
+// Database stuff
+var db = require("./models");
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const bodyParser = require("body-parser");
+const path = require("path");
+const bcrypt = require("bcryptjs");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).json(`Missing ${!email ? "email" : "password"}!`);
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+    await db("users").insert({ email: email, hash: hash });
+
+    res.status(200).json("All good!");
+  } catch (e) {
+    // console.log(e); // Uncomment if needed for debug
+    // If a SQLITE_CONSTRAINT has been violated aka. row with that email already exists. You can read more: https://www.sqlite.org/c3ref/c_abort.html
+    if (e.errno === 19) {
+      res.status(400).json("A user with that email already exists!");
+    } else {
+      res.status(400).json("Something broke!");
+    }
+  }
 });
 
 module.exports = router;
