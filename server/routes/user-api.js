4651e05b64a1ee79e4bd6a6af4f14b9903a6412b
tim <timothy.potts@colorado.edu>
2020-07-25 13:20:32 -0600

    ditching express generated backend, just using a node server that I'm more familiar with.

---
+++ server/routes/user-api.js
@@ -0,0 +1,73 @@
+var express = require("express");
+// var router = express.Router();
+var bodyParser = require("body-parser");
+var path = require("path");
+var bcrypt = require("bcryptjs");
+
+//Connect to Database
+var db = require("../models");
+module.exports = function (app) {
+  // Middleware
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
+  /* GET users listing. */
+  // app.get("/", function (req, res, next) {
+  //   res.send("respond with a resource");
+  // });
+
+  app.post("/register", async (req, res) => {
+    try {
+      const { email, password, username } = req.body;
+
+      if (!email || !password) {
+        res.status(400).json(`Missing ${!email ? "email" : "password"}!`);
+        return;
+      }
+
+      const hash = await bcrypt.hash(password, 10);
+      await db.user
+        .create({
+          username: username,
+          email: email,
+          password: hash,
+        })
+        .then(function (forumDB) {
+          res.json(forumDB);
+          return;
+        });
+
+      // return res.status(200).json("All good!");
+    } catch (e) {
+      console.log(e); // Uncomment if needed for debug
+      // If a SQLITE_CONSTRAINT has been violated aka. row with that email already exists. You can read more: https://www.sqlite.org/c3ref/c_abort.html
+      if (e.errno === 19) {
+        res.status(400).json("A user with that email already exists!");
+        return;
+      } else {
+        res.status(400).json("Something broke!");
+        return;
+      }
+    }
+  });
+
+  app.post("/login", async (req, res) => {
+    try {
+      const [email, password] = req.body;
+      const user = await db.user.first("*").where({ email: email });
+      if (user) {
+        const validPass = await bcrypt.compare(password, user.hash);
+        if (validPass) {
+          res.status(200).json("Valid Email and Passowrd!");
+        } else {
+          res.json("Wrong password");
+        }
+      } else {
+        res.status(404).json("User not found");
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).send("Something broke");
+    }
+  });
+};
---
+++ server/server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+const bodyparser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Allows connection
+var cors = require("cors");
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cors());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/user-api.js")(app);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+});
